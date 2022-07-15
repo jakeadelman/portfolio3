9c127fd5c99940a80673e36d9c07aee8e49d434d
Jacob Adelman <jacobzadelman@gmail.com>
2022-07-15 02:57:29 -0400

    change nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,85 +1,35 @@
 import React from "react";
 
 export default function nav({ id, show, showFunc }) {
-  if (id == "1") {
-    if (show == true) {
-      return (
-        <nav className="small-nav">
-          <li>
-            <a href="#outer" id="1" onClick={(e) => showFunc(e)}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" id="2" onClick={(e) => showFunc(e)}>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#contact" id="3" onClick={showFunc}>
-              Contact
-            </a>
-          </li>
-        </nav>
-      );
-    } else {
-      return (
-        <nav className="small-nav">
-          <li>
-            <a href="#outer" id="1" onClick={(e) => showFunc(e)}>
-              Home
-            </a>
-          </li>
-        </nav>
-      );
-    }
-  }
-  if (id == "2") {
-    if (show == true) {
-      return (
-        <nav className="small-nav">
-          <li>
-            <a href="#portfolio" id="2" onClick={(e) => showFunc(e)}>
-              Portfolio
-            </a>
-          </li>
-          <li className="first">
-            <a href="#outer" id="1" onClick={(e) => showFunc(e)}>
-              Home
-            </a>
-          </li>
-
-          <li>
-            <a href="#contact" id="3" onClick={showFunc}>
-              Contact
-            </a>
-          </li>
-        </nav>
-      );
-    } else {
-      return (
-        <nav className="small-nav">
-          <li>
-            <a href="#portfolio" id="2" onClick={(e) => showFunc(e)}>
-              Portfolio
-            </a>
-          </li>
-        </nav>
-      );
-    }
-  }
-  if (id == "3") {
-    <nav>
-      <li className="first">
-        <a href="#contact">Contact</a>
-      </li>
-      ;
-      <li>
-        <a href="#outer">Home</a>
-      </li>
-      <li>
-        <a href="#portfolio">Portfolio</a>
-      </li>
-    </nav>;
+  if (show == true) {
+    return (
+      <nav className="small-nav">
+        <li ClassName="fist smallnav">
+          <a href="#outer" id="1" onClick={(e) => showFunc(e)}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" id="2" onClick={(e) => showFunc(e)}>
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a href="#contact" id="3" onClick={showFunc}>
+            Contact
+          </a>
+        </li>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="small-nav">
+        <li className="first smallnav">
+          <a href="#outer" id="1" onClick={(e) => showFunc(e)}>
+            Home
+          </a>
+        </li>
+      </nav>
+    );
   }
 }
